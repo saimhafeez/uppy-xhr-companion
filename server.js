d4bb44484517360aaeb9797db7d665e74be8b0f3
Saim Hafeez <73340622+saimhafeez@users.noreply.github.com>
2025-11-06 14:27:19 +0500

    added scope adjustments for google meet

--- server.js
+++ server.js
@@ -1217,6 +1217,7 @@ app.get('/login/google/meet', (req, res) => {
       'https://www.googleapis.com/auth/userinfo.email',
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/calendar', // Required for Meet management
+      'https://www.googleapis.com/auth/meetings.space.created' // To Create and Manage Spaces
     ]
   });
   res.redirect(url);
