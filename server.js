df8f73c9311166063a06e5b7070ea33360af491e
Saim Hafeez <73340622+saimhafeez@users.noreply.github.com>
2026-05-22 20:01:55 +0500

    BUBBLE_RSVP_ENDPOINT updated to test-version-initialize

--- server.js
+++ server.js
@@ -3816,7 +3816,7 @@ app.post('/webhooks/sendgrid/inbound_parse', (req, res) => {
       console.log(`>>> FINAL PAYLOAD TO BUBBLE: RSVP ${status} | UID ${unique_id} | EMAIL ${finalAttendeeEmail} <<<`);
 
             // 6. Forward to Bubble
-      const BUBBLE_RSVP_ENDPOINT = "https://upward.page/api/1.1/wf/update_ical_rsvp";
+      const BUBBLE_RSVP_ENDPOINT = "https://upward.page/version-test/api/1.1/wf/update_ical_rsvp/initialize"; 
       
       await fetch(BUBBLE_RSVP_ENDPOINT, {
         method: "POST",
